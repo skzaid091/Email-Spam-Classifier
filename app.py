225ca484a88cab4f378a1d03b2032f579922edc9
skzaid091 <117260335+skzaid091@users.noreply.github.com>
2023-03-24 08:47:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import pickle
+import streamlit as st
+import nltk
+from nltk.corpus import stopwords
+import string
+from nltk.stem import PorterStemmer
+
+st.set_page_config(layout='wide')
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load((open('model.pkl', 'rb')))
+stop_words = stopwords.words('english')
+ps = PorterStemmer()
+
+
+def empty_space():
+    col4, col5, col6 = st.columns(3)
+    with col4:
+        st.write('')
+        st.write('')
+        st.write('')
+    with col5:
+        pass
+    with col6:
+        pass
+
+
+def transform(obj):
+    obj = obj.lower()
+    obj = nltk.word_tokenize(obj)
+
+    x = []
+    for i in obj:
+        if i.isalnum():
+            x.append(i)
+
+    y = x.copy()
+    x.clear()
+
+    for i in x:
+        if i not in stop_words and i not in string.punctuation:
+            y.append(ps.stem(i))
+
+    return ' '.join(y)
+
+
+st.title('Email Spam Detection')
+
+empty_space()
+
+inp = st.text_input('Enter the Message / Email')
+
+
+empty_space()
+
+btn = st.button('Predict')
+
+empty_space()
+
+if btn:
+    transformed_inp = transform(inp)
+    data = tfidf.transform([transformed_inp])
+    result = model.predict(data)[0]
+
+    if result == 1:
+        st.subheader('Spam')
+    else:
+        st.subheader('Not Spam')
